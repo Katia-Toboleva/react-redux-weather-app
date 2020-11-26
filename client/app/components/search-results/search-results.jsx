159c7531b7897a205cadcf81243842938c4feeea
Katia-2020 <kateflights@gmail.com>
2020-11-26 12:59:22 +0000

    Search Results component

---
+++ client/app/components/search-results/search-results.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './search-results.scss';
+
+const SearchResults = () => (
+  <div className={styles['search-results']}>
+
+  </div>
+);
+
+export default SearchResults;
