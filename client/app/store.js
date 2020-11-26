95a5c7def78606dfeeea99ba602bd18798180949
Katia-2020 <kateflights@gmail.com>
2020-11-26 21:11:31 +0000

    redux thunk added

--- client/app/store.js
+++ client/app/store.js
@@ -1,11 +1,14 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
+
 import reducers from './store.reducers';
 
 const store = configureStore({
   reducer: combineReducers(reducers),
   middleware: [
     createLogger(),
+    thunk,
   ],
 });
 
