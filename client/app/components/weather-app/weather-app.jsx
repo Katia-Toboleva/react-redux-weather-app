724895e80312d981596fd1999371465d79bd5181
Katia-2020 <kateflights@gmail.com>
2020-11-26 21:14:27 +0000

    moved the fetch request logic to separate files

--- client/app/components/weather-app/weather-app.jsx
+++ client/app/components/weather-app/weather-app.jsx
@@ -1,54 +1,49 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from '@reduxjs/toolkit';
+
 import styles from './weather-app.scss';
 import SearchResults from '../search-results';
 import SearchField from '../search-field';
 
-class WeatherApp extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+import actions from './state/weather-app.actions';
 
-  componentDidMount() {
-    console.log('working');
-    this.fetchWeather();
-  }
+const Weather = ({ onChange, onSubmit, data }) => (
+  <div className={styles['weather-app']}>
+    <div className={styles['weather-app__wrapper']}>
+      <SearchResults items={data} />
+      <SearchField
+        onChange={onChange}
+        onSubmit={onSubmit}
+      />
+    </div>
+  </div>
+);
 
-  fetchWeather() {
-    this.fetchWeatherPending();
+const WeatherContainer = (props) => {
+  const handleWeatherInputChange = () => {};
 
-    fetch('https://community-open-weather-map.p.rapidapi.com/weather?q=London', {
-      method: 'GET',
-      headers: {
-        'x-rapidapi-key': '1309b0fb73msh701dd5b07f4d0f1p1bdbf5jsnaab2ae4a11ba',
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-      },
-    })
-      .then(response => response.json())
-      .then(data => this.fetchWeatherSuccess(data))
-      .catch(() => this.fetchWeatherRejected());
-  }
+  const handleSubmit = () => {
+    props.actions.fetchWeather();
+  };
 
-  fetchWeatherSuccess(data) {
-    console.log(data);
-  }
+  return (
+    <Weather
+      onChange={handleWeatherInputChange}
+      onSubmit={handleSubmit}
+    />
+  );
+};
 
-  fetchWeatherPending() {
-  }
+const mapStateToProps = (store, ownProps) => ({
+  ...ownProps,
+  state: {
+    ...store.weather,
+  },
+});
 
-  fetchWeatherRejected() {
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(actions, dispatch),
+});
 
-  }
-
-  render() {
-    return (
-      <div className={styles['weather-app']}>
-        <div className={styles['weather-app__wrapper']}>
-          <SearchResults />
-          <SearchField />
-        </div>
-      </div>
-    );
-  }
-}
-
-export default WeatherApp;
+export default connect(mapStateToProps, mapDispatchToProps)(WeatherContainer);
