1cc04ef9ddb4796ee3ba745433dadab9ef8c4bf5
Katia-2020 <kateflights@gmail.com>
2020-11-26 13:10:40 +0000

    conditions component

---
+++ client/app/components/conditions/conditions.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './conditions.scss';
+
+const Conditions = () => (
+  <div className={styles.conditions}>
+
+  </div>
+);
+
+export default Conditions;
