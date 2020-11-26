5e608a53136a4e57264ca66e9e485f34ae6d190f
Katia-2020 <kateflights@gmail.com>
2020-11-26 12:29:46 +0000

    location input component

---
+++ client/app/components/location-input/location-input.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './location-input.scss';
+
+const LocationInput = () => (
+  <input className={styles['location-input']} placeholder="Location..." />
+);
+
+export default LocationInput;
