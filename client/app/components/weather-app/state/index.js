ddfc92434ded344ee627ed39454b6c1897daae82
Katia-2020 <kateflights@gmail.com>
2020-11-26 15:44:43 +0000

    added exports for state in weather app

---
+++ client/app/components/weather-app/state/index.js
@@ -0,0 +1,3 @@
+export { default as reducer } from './weather-app.reducers';
+export { default as constants } from './weather-app.constants';
+export { default as actions } from './weather-app.actions';
