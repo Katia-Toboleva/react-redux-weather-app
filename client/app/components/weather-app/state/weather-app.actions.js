c4069c41b9ee7236a1a18549c2c4bf6b1d682d28
Katia-2020 <kateflights@gmail.com>
2020-11-26 21:13:07 +0000

    moved the logic of async request to API from component to actions

--- client/app/components/weather-app/state/weather-app.actions.js
+++ client/app/components/weather-app/state/weather-app.actions.js
@@ -1,8 +1,13 @@
 import CONSTANTS from './weather-app.constants';
+import WeatherApi from './weather-app.api';
 
 const fetchWeatherSuccess = (data) => ({
   type: CONSTANTS.FETCH_WEATHER_SUCCESS,
-  payload: data,
+  payload: {
+    location: data.name,
+    temperature: data.main.temp,
+    conditions: data.weather[0].main,
+  },
 });
 
 const fetchWeatherPending = () => ({
@@ -13,8 +18,30 @@ const fetchWeatherRejected = () => ({
   type: CONSTANTS.FETCH_WEATHER_REJECTED,
 });
 
+// async
+const fetchWeather = () => (dispatch, getState) => {
+  const { weather } = getState();
+
+  const success = data => {
+    dispatch(fetchWeatherSuccess(data));
+  };
+
+  const rejected = () => {
+    dispatch(fetchWeatherRejected());
+  };
+
+  const queryParams = {
+    city: weather.city,
+  };
+
+  dispatch(fetchWeatherPending());
+
+  return WeatherApi.get(queryParams)
+    .then(response => response.json())
+    .then(success)
+    .catch(rejected);
+};
+
 export default {
-  fetchWeatherSuccess,
-  fetchWeatherPending,
-  fetchWeatherRejected,
+  fetchWeather,
 };
