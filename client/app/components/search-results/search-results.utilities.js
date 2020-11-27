29a25ee97b108c3a2aac4f34b5184e11af2ae4f5
Katia-2020 <kateflights@gmail.com>
2020-11-27 22:18:26 +0000

    added logic for correct temperature display

--- client/app/components/search-results/search-results.utilities.js
+++ client/app/components/search-results/search-results.utilities.js
@@ -3,8 +3,8 @@ export const calcTemperatureValue = (tempType, temperature) => {
   const farenheit = Math.round((temperature * 9) / 5 - 459.67);
 
   switch (tempType) {
-    case 'metric': return celcium;
-    case 'imperial': return farenheit;
-    default: return celcium;
+    case 'metric': return (celcium > 0 ? `+${celcium}°` : `${celcium}°`);
+    case 'imperial': return (farenheit > 0 ? `+${farenheit}` : farenheit);
+    default: return (celcium > 0 ? `+${celcium}°` : `${celcium}°`);
   }
 };
