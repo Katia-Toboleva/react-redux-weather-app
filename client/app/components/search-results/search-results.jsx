2f65f621bcafb8d0cb74796d4770fde515b8e7ab
Katia-2020 <kateflights@gmail.com>
2020-11-27 21:43:04 +0000

    added logic for SearchResults

--- client/app/components/search-results/search-results.jsx
+++ client/app/components/search-results/search-results.jsx
@@ -4,21 +4,35 @@ import { Row, Column } from '../grid';
 import Conditions from '../conditions';
 import Temperature from '../temperature';
 import TemperatureToggles from '../temperature-toggles';
+import { calcTemperatureValue } from './search-results.utilities';
 
-const SearchResults = ({ onSwitch }) => (
-  <div className={styles['search-results']}>
-    <Row direction="row" position="center">
-      <Column>
-        <Conditions />
-      </Column>
-      <Column>
-        <Temperature />
-      </Column>
-      <Column>
-        <TemperatureToggles onSwitch={onSwitch} />
-      </Column>
-    </Row>
-  </div>
-);
+const SearchResults = ({ data, onSwitch }) => {
+  const {
+    conditions,
+    fetchWeatherRequestStatus,
+    temperature,
+    tempType,
+  } = data;
+
+  return (
+    (fetchWeatherRequestStatus === 'pending' && <div>loading</div>) ||
+    (fetchWeatherRequestStatus === 'rejected' && <div>server is not responding</div>) ||
+    (fetchWeatherRequestStatus === 'success' && (
+      <div className={styles['search-results']}>
+        <Row direction="row" position="center">
+          <Column shrink>
+            <Conditions conditions={conditions} />
+          </Column>
+          <Column grow>
+            <Temperature temperature={calcTemperatureValue(tempType, temperature)} />
+          </Column>
+          <Column shrink>
+            <TemperatureToggles onSwitch={onSwitch} />
+          </Column>
+        </Row>
+      </div>
+    ))
+  );
+};
 
 export default SearchResults;
