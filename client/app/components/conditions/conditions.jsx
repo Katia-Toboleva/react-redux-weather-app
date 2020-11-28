33d1ec35433ee7ca92c0bc485489c54083fca1c6
Katia-2020 <kateflights@gmail.com>
2020-11-28 15:24:39 +0000

    conditions component

--- client/app/components/conditions/conditions.jsx
+++ client/app/components/conditions/conditions.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './conditions.scss';
+import Text from '../text';
 
-const Conditions = () => (
+const Conditions = ({ conditions }) => (
   <div className={styles.conditions}>
-
+    <Text text={conditions} center size="small" color="dark-blue" weight="400" />
   </div>
 );
 
