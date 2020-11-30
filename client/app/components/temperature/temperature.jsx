85e477ecf0dfbccbab3135a79c6c42f0459b5fbf
Katia-2020 <kateflights@gmail.com>
2020-11-30 19:10:19 +0000

    updated Temperature component

--- client/app/components/temperature/temperature.jsx
+++ client/app/components/temperature/temperature.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import styles from './temperature.scss';
+import Text from '../text';
 
 const Temperature = ({ temperature }) => (
   <div className={styles.temperature}>
-    {temperature}
+    <Text text={temperature} color="dark-blue" size="x-large" weight="700" />
   </div>
 );
 
