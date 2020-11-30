d3c0160af6b093865fb4dbc58ca73da4cb902ce0
Katia-2020 <kateflights@gmail.com>
2020-11-30 12:41:06 +0000

    changed images

--- README.md
+++ README.md
@@ -1,1 +1,55 @@
 # react-redux-weather-app
+
+
+{location: {…}, current: {…}}
+current:
+cloud: 75
+condition: {text: "Partly cloudy", icon: "//cdn.weatherapi.com/weather/64x64/night/116.png", code: 1003}
+feelslike_c: 8
+feelslike_f: 46.4
+gust_kph: 2.2
+gust_mph: 1.3
+humidity: 93
+is_day: 0
+last_updated: "2020-11-30 10:45"
+last_updated_epoch: 1606729517
+precip_in: 0
+precip_mm: 0
+pressure_in: 30.8
+pressure_mb: 1025
+temp_c: 8
+temp_f: 46.4
+uv: 1
+vis_km: 10
+vis_miles: 6
+wind_degree: 90
+wind_dir: "E"
+wind_kph: 6.1
+wind_mph: 3.8
+__proto__: Object
+
+
+
+location:
+country: "Spain"
+lat: 43.25
+localtime: "2020-11-30 11:09"
+localtime_epoch: 1606730987
+lon: -2.97
+name: "Bilbao"
+region: "Pais Vasco"
+tz_id: "Europe/Madrid"
+
+
+payload: {
+    location: data.location.name,
+    temperature: {
+      metric: data.current.temp_c,
+      imperial: data.current.temp_f,
+    },
+    conditions: {
+      condition: data.current.condition.text,
+      conditionCode: data.current.condition.code,
+      isDay: data.current.is_day,
+    },
+  },
--- client/app/components/weather-app/images/index.js
+++ client/app/components/weather-app/images/index.js
@@ -1,33 +1,74 @@
-import ash from './ash.jpg';
+import blizzard from './blizzard.jpg';
+import blowingSnow from './blowing-snow.jpg';
 import clear from './clear.jpg';
 import clouds from './clouds.jpg';
 import drizzle from './drizzle.jpg';
-import dust from './dust.jpg';
 import fog from './fog.jpg';
-import haze from './haze.jpg';
+import hail from './hail.jpg';
 import mist from './mist.jpg';
+import heavyRain from './heavy-rain.jpg';
+import icePellets from './ice-pellets.jpg';
+import lightSnow from './light-snow.jpg';
+import lightRain from './light-rain.jpg';
+import moderateRain from './moderate-rain.jpg';
+import moderateSnow from './moderate-snow.jpg';
+import overcast from './overcast.jpg';
+import partlyCloudy from './partly-cloudy.jpg';
 import rain from './rain.jpg';
-import sand from './sand.jpg';
-import smoke from './smoke.jpg';
+import showers from './showers.jpg';
+import sleet from './sleet.jpg';
 import snow from './snow.jpg';
-import squall from './squall.jpg';
 import thunderstorm from './thunderstorm.jpg';
-import tornado from './tornado.jpg';
 
-export {
-  ash,
-  clear,
-  clouds,
-  drizzle,
-  dust,
-  fog,
-  haze,
-  mist,
-  rain,
-  sand,
-  smoke,
-  snow,
-  squall,
-  thunderstorm,
-  tornado,
+const weatherConditions = {
+  1000: clear,
+  1003: partlyCloudy,
+  1006: clouds,
+  1009: overcast,
+  1030: mist,
+  1063: rain,
+  1066: snow,
+  1069: sleet,
+  1072: drizzle,
+  1087: thunderstorm,
+  1114: blowingSnow,
+  1117: blizzard,
+  1135: fog,
+  1147: fog,
+  1150: drizzle,
+  1153: drizzle,
+  1168: drizzle,
+  1171: drizzle,
+  1180: lightRain,
+  1183: lightRain,
+  1186: moderateRain,
+  1189: moderateRain,
+  1192: heavyRain,
+  1195: heavyRain,
+  1198: heavyRain,
+  1201: rain,
+  1204: sleet,
+  1207: sleet,
+  1210: lightSnow,
+  1213: lightSnow,
+  1216: moderateSnow,
+  1219: moderateSnow,
+  1222: snow,
+  1225: snow,
+  1237: icePellets,
+  1240: rain,
+  1243: heavyRain,
+  1246: showers,
+  1249: showers,
+  1252: showers,
+  1255: moderateSnow,
+  1258: moderateSnow,
+  1261: hail,
+  1264: hail,
+  1273: thunderstorm,
+  1276: thunderstorm,
+  1279: snow,
+  1282: snow,
 };
+
+export default weatherConditions;
